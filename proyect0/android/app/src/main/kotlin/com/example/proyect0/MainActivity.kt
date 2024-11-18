a4d99b59ef8017eb22280046eee2f9fdd4d0afee
matias <151059997+matiasgerr@users.noreply.github.com>
2024-11-18 18:08:17 -0300

    agregando proyecto de flutter

---
+++ proyect0/android/app/src/main/kotlin/com/example/proyect0/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.proyect0
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ proyect0/flutter_application_0/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_0/src/pages/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+//entrada de toda la app
